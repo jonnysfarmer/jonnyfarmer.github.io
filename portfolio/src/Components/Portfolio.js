e1b01402deb21826ad05554e1bef8c1de537f655
jonnysfarmer <john.farmer@latimerross.com>
2020-01-19 20:52:23 +0000

    update

--- portfolio/src/Components/Portfolio.js
+++ portfolio/src/Components/Portfolio.js
@@ -2,7 +2,6 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Stepper from "@material-ui/core/Stepper";
 import Step from "@material-ui/core/Step";
-import StepLabel from "@material-ui/core/StepLabel";
 import StepContent from "@material-ui/core/StepContent";
 import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
